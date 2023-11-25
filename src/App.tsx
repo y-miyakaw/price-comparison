f838cd4504510f6b813332779edb5c2cf4202eb3
y-miyakaw <y-miyakawa@tribeat.com>
2023-11-25 20:07:48 +0900

    create docker

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <table>
+        <tr>
+          <td>
+            商品1
+          </td>
+          <td>
+            <input name="volume1">
+            </input>
+          </td>
+          <td>
+            <input name="price1">
+            </input>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            商品2
+          </td>
+          <td>
+            <input name="volume2">
+            </input>
+          </td>
+          <td>
+            <input name="price2">
+            </input>
+          </td>
+        </tr>
+      </table>
+    </>
   );
 }
 
